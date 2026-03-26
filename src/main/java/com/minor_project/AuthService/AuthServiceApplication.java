bb6b0b48dc7ed6a824e44f403e6d6acefdfb2fc3
minorproject-slg <minorproject.slg@gmail.com>
2026-03-26 22:04:48 +0530

    added database url

--- src/main/java/com/minor_project/AuthService/AuthServiceApplication.java
+++ src/main/java/com/minor_project/AuthService/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.minor_project.AuthService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.minor_project"})
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
